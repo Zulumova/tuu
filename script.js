dd5874604fdb4a7bc8550930863d9e5c75cce5c7
Zulumova <166822473+Zulumova@users.noreply.github.com>
2024-04-12 16:40:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//console.log( 'zhumagul')
+let sum = 400%89
+let sum2 = 6-2
+console.log (sum )
+console.log(sum2)
+
+// const userAnswer =confirm(' Сен 18 жаштасынбы')
+// console.log( userAnswer ?)
+// 
+// const userAnswer = prompt (' сенин атын ким ')
+// console.log( userAnswer)
+// 
+// const userAnswer= prompt (' Канча жаштасын ?'
+// )
+// if(userAnswer >= 18){
+//     console.log(
+//         ' жашын жетет')
+        
+// } else {
+//     console.log(' жашын жетпейт')
+    
+// } 
+  
+// const number=1990
+// if ( number===1991){
+//     console.log(' tuura joop')
+// } else if( number ===1990) {
+//     console.log (' jakyndady')
+
+// } else if(
+//     number=== 1992
+// ) {
+//     console.log(' bir az jakyndady')
+// }
+// const number1 =1999
+// const answer = number1 ===1990? " tuura" : " tuura emes"
+// console.log( answer) 
+const number=10
+if (number> 0 & number ===10) {
+    console.log (" tuura ")
+} else {
+    console.log (" tuura emes")
+}
+
